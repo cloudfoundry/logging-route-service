e5bbcc4fbc22ddf7b4c4466f19996c222910e8f3
Chris Piraino and Mark St.Godard <markstgodard@gmail.com>
2015-07-07 11:58:03 -0700

    Log response from backend.
    - Refactor request logging.
    - Use custom transport to log response from backend.
    
    [#96033478]

--- main.go
+++ main.go
@@ -24,30 +24,25 @@ func main() {
 
 	log.SetOutput(os.Stdout)
 
-	proxy := NewProxy()
+	roundTripper := NewLoggingRoundTripper()
+	proxy := NewProxy(roundTripper)
 
 	log.Fatal(http.ListenAndServe(":"+port, proxy))
 }
 
-func NewProxy() http.Handler {
+func NewProxy(transport http.RoundTripper) http.Handler {
 	reverseProxy := &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
 			forwardedURL := req.Header.Get(CF_FORWARDED_URL_HEADER)
-
-			log.Println("Received request: ")
-			log.Printf("%s: %s\n", CF_FORWARDED_URL_HEADER, forwardedURL)
-			log.Printf("%s: %s\n", CF_PROXY_SIGNATURE_HEADER, req.Header.Get(CF_PROXY_SIGNATURE_HEADER))
-			log.Println("")
-			log.Printf("Headers: %#v\n", req.Header)
-			log.Println("")
+			sigHeader := req.Header.Get(CF_PROXY_SIGNATURE_HEADER)
 
 			body, err := ioutil.ReadAll(req.Body)
 			if err != nil {
 				log.Fatalln(err.Error())
 			}
-			log.Printf("Request Body: %s\n", string(body))
-
 			req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+			logRequest(forwardedURL, sigHeader, req.Header, string(body))
 
 			// Note that url.Parse is decoding any url-encoded characters.
 			url, err := url.Parse(forwardedURL)
@@ -55,11 +50,56 @@ func NewProxy() http.Handler {
 				log.Fatalln(err.Error())
 			}
 
-			log.Printf("Forwarding to: %s\n", forwardedURL)
-
 			req.URL = url
 			req.Host = url.Host
 		},
+		Transport: transport,
 	}
 	return reverseProxy
+}
+
+func logRequest(forwardedURL, sigHeader string, headers http.Header, body string) {
+	log.Println("Received request: ")
+	log.Printf("%s: %s\n", CF_FORWARDED_URL_HEADER, forwardedURL)
+	log.Printf("%s: %s\n", CF_PROXY_SIGNATURE_HEADER, sigHeader)
+	log.Println("")
+	log.Printf("Headers: %#v\n", headers)
+	log.Println("")
+	log.Printf("Request Body: %s\n", body)
+}
+
+type LoggingRoundTripper struct {
+	transport http.RoundTripper
+}
+
+func NewLoggingRoundTripper() *LoggingRoundTripper {
+	return &LoggingRoundTripper{
+		transport: http.DefaultTransport,
+	}
+}
+
+func (lrt *LoggingRoundTripper) RoundTrip(request *http.Request) (*http.Response, error) {
+	var err error
+	var res *http.Response
+
+	log.Printf("Forwarding to: %s\n", request.URL.String())
+	res, err = lrt.transport.RoundTrip(request)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Println("")
+	log.Printf("Response Headers: %#v\n", res.Header)
+	log.Println("")
+	log.Printf("Response Body: %s\n", string(body))
+	log.Println("")
+	res.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	log.Println("Sending response to GoRouter...")
+
+	return res, err
 }
